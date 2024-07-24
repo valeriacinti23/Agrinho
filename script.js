89561aae1cbb0b19b9975ad4d2e93b15b68d0bd1
valeriacinti23 <140653995+valeriacinti23@users.noreply.github.com>
2024-07-24 08:18:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function comprar(){
+    const width = 800;
+    const height = 600;
+    const left = (screen.width/2)-(width/2);
+    const top = (screen.height/2)-(height/2);
+    window.open('pagamento.html', 'Pagamento',
+    `width=${width},height=${height},${top},left=${left}`);
+}
